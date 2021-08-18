a31544dfcf8db244e9eb178ce36f3f34fd159635
eainde <akshay.dipta@gmail.com>
2021-08-18 16:16:25 +0100

    Docker file

---
+++ kafka-producer/src/main/java/com/eainde/kafka/producer/Application.java
@@ -0,0 +1,11 @@
+package com.eainde.kafka.producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ kafka-producer/src/main/java/com/eainde/kafka/producer/channel/OrderChannel.java
@@ -0,0 +1,9 @@
+package com.eainde.kafka.producer.channel;
+
+import org.springframework.cloud.stream.annotation.Output;
+import org.springframework.messaging.MessageChannel;
+
+public interface OrderChannel {
+    @Output("output-order")
+    MessageChannel output();
+}
